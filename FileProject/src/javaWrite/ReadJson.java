7bc6269a1523818049ca403be08196a2aa44267f
jackdopark <jackdopark8@gmail.com>
2022-06-27 12:16:32 +0900

    .

--- FileProject/src/javaWrite/ReadJson.java
+++ FileProject/src/javaWrite/ReadJson.java
@@ -7,6 +7,9 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Iterator;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 public class ReadJson {
 	//json
 
@@ -25,9 +28,18 @@ public class ReadJson {
 				String s = br.readLine();
 				if( s == null ) break;
 				result += s;
-				//map = 키값과 데이
+				//map = 키값과 데이터 값을 다 갖는다
 			}
-			System.out.println(result);
+			//System.out.println(result);
+			//json으로 변환해서 출력
+			JSONArray arr = new JSONArray(result);
+//			System.out.println(arr.length());
+			for (int i = 0; i < arr.length(); i++) {
+//				System.out.println(arr.get(i));
+				JSONObject obj = arr.getJSONObject(i);
+				System.out.println(obj.get("id")+ " " + obj.get("first_name")+ " " +
+				obj.get("last_name")+ " " + obj.get("gender")+ " " + obj.get("ip_address")+" " + obj.get("email"));
+			}
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
---
+++ FileProject/src/javaWrite/ReadJson3.java
@@ -0,0 +1,57 @@
+package javaWrite;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class ReadJson3 {
+	//data.json 파일에서 성별이 남자고 Id 값이 3의 배수인 사람만 조회한다.
+
+	public static void main(String[] args) {
+		File file = new File("data.json");
+		FileReader fr = null;
+		BufferedReader br = null;
+		
+		
+		try {
+			fr = new FileReader(file);
+			br = new BufferedReader(fr);
+			String r = new String();
+			
+			while(true) {
+				String s = br.readLine();
+				if( s == null ) break;
+				r += s;
+			}
+			JSONArray arr = new JSONArray(r); //json으로 바꾼부분
+			for (int i = 0; i < arr.length(); i++) {
+				JSONObject obj = arr.getJSONObject(i);
+				if(obj.get("gender").equals("Male") && obj.getInt("id") % 3 == 0 ){
+					System.out.println(obj.get("id")+ " " + obj.get("first_name")+ " " +obj.get("last_name")+ " " + obj.get("gender")+ " " + obj.get("ip_address")+" " + obj.get("email"));					
+				}
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			try {
+				if(br != null) br.close();
+				if(fr != null) fr.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		
+	}
+	}
+	
+
+}
