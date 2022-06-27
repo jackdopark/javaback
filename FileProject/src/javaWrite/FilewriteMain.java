528e9edf7e89052b12402f43751e48ccfc30ff02
jackdopark <jackdopark8@gmail.com>
2022-06-27 09:50:55 +0900

    .

---
+++ FileProject/src/javaWrite/FilewriteMain.java
@@ -0,0 +1,13 @@
+package javaWrite;
+
+public class FilewriteMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//1 파일 오픈
+		//2 스트림 생성(입력/출력)
+		//3 입출력 수행
+		//4 스트림 닫기
+	}
+
+}
