695cc489e4a5a94ea6fd217e37e56123f0cf0348
jackdopark <jackdopark8@gmail.com>
2022-06-27 10:23:29 +0900

    .

---
+++ FileProject/src/javaWrite/FIleReadMain.java
@@ -0,0 +1,13 @@
+package javaWrite;
+
+public class FIleReadMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//1. 파일 오픈 - write.txt
+		//2. 스트림 생성 ,fileReader, bufferedReader
+		//3. 파일 내용 읽기
+		//4. 스트림 닫
+	}
+
+}
--- FileProject/src/javaWrite/FilewriteMain.java
+++ FileProject/src/javaWrite/FilewriteMain.java
@@ -1,13 +1,44 @@
 package javaWrite;
 
+import java.io.File;
+//import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class FilewriteMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		//1 파일 오픈
+		File file = new File("write.txt");
 		//2 스트림 생성(입력/출력)
-		//3 입출력 수행
-		//4 스트림 닫기
+		FileWriter fw = null; //전역 변수로 만들어주기 위해서 fileWriter를 밖에서 선언해준다.
+		PrintWriter pw =null;
+		try {
+			fw = new FileWriter(file); //한글에 유리하다
+			//FileOutputStream fos;//바이트 기준(문자)
+			pw = new PrintWriter(file);
+			
+			//3 입출력 수행
+			pw.println("Hello Wolrd");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			try {
+				//4 스트림 닫기
+				if(pw != null )pw.close();
+				if(fw != null )fw.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} 
+
+		
+		
+		
+		
+		
 	}
 
 }
